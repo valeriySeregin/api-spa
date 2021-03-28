00584de959a90f2103a11486962d50d3057a9fec
Valeriy Seregin <valera-s-38@mail.ru>
2021-03-28 22:25:24 +0300

    add factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
+use App\Models\News;
 
 class UserFactory extends Factory
 {
@@ -14,6 +14,7 @@ class UserFactory extends Factory
      * @var string
      */
     protected $model = User::class;
+    protected $news = News::class;
 
     /**
      * Define the model's default state.
@@ -24,10 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'rating' => rand(1, 100),
         ];
     }
 
