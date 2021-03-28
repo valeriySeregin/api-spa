d3b51ff5527fa1a26b141a898dfe18ea95ce9fca
Valeriy Seregin <valera-s-38@mail.ru>
2021-03-28 22:20:59 +0300

    modify migrations

--- database/migrations/2021_03_28_130653_create_news_table.php
+++ database/migrations/2021_03_28_130653_create_news_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateNewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('rating');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->text('short_description');
+            $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
         });
     }
 
@@ -28,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news');
     }
 }
