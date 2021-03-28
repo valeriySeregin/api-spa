ad5d0f685b8db5177d9dd29daa5050066a0426fa
Valeriy Seregin <valera-s-38@mail.ru>
2021-03-28 22:28:46 +0300

    create routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('news', [NewsController::class, 'index'])->name('news.index');
+
+Route::get('/news/{id}', function () {
+    return var_dump('welcome to news watching page!');
+});
