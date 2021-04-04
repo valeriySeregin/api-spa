36b44061a1300511f5205c6e0f135031173ca03c
Valeriy Seregin <valera-s-38@mail.ru>
2021-04-04 17:11:58 +0300

    create logic for data fetching from DB

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,13 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\News;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
     public function index()
     {
-        $news = News::paginate();
+        $news = DB::table('news')
+            ->leftJoin('users', 'users.id', '=', 'news.user_id')
+            ->paginate(12);
 
         return view('news.index', compact('news'));
+    }
+
+    public function show($id)
+    {
+        $singleNewsItem = News::findOrFail($id);
+        $user = User::findOrFail($singleNewsItem->user_id);
+        $userId = $user->id;
+        $userNewsCount = DB::table('news')
+            ->select(DB::raw('COUNT(id), user_id'))
+            ->where('user_id', $userId)
+            ->groupBy('user_id')
+            ->get()[0]
+            ->count;
+
+        return view('news.show', compact('singleNewsItem', 'user', 'userNewsCount'));
     }
 }
